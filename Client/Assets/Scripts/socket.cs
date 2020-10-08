c57501763fe5e40f32f1bf546b0b5793b09a331b
Håvard Markhus <haavasma@stud.ntnu.no>
2020-10-08 02:38:03 +0200

    set up simple networking system

---
+++ Client/Assets/Scripts/socket.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using GameNetWorkClient;
+
+public class socket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SocketClient client;
+
+    private GameObject cubes;
+
+    public bool myTurn = false;
+
+    void Start()
+    {
+        cubes = GameObject.Find("Cubes");
+        client = new SocketClient();
+        client.queue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myTurn){
+            for (int i = 0; i < cubes.transform.childCount; i++)
+            {
+                client.SendTransform(cubes.transform.GetChild(i).name, cubes.transform.GetChild(i).transform);
+            }
+        }
+        else {
+            Dictionary<string, FormattedTransform> transforms = client.getTransforms();
+
+            foreach (KeyValuePair<string, FormattedTransform> entry in transforms)
+            {
+                GameObject piece = GameObject.Find(entry.Key);
+                piece.transform.position = entry.Value.position;
+                piece.transform.rotation = entry.Value.rotation;
+                piece.transform.localScale = entry.Value.scale;
+            }
+
+            if(client.getChannel() != -1){
+                myTurn = true;
+            }
+
+/*
+            Transform piece = cubes.transform.GetChild(1).transform;
+
+            piece.position = new Vector3(transforms["Cube (1)"].position.x, transforms["Cube (1)"].position.y, transforms["Cube (1)"].position.z - 10.0f);
+            piece.rotation = transforms["Cube (1)"].rotation;
+            piece.localScale = transforms["Cube (1)"].scale;
+            */
+        }
+    }
+
+}
--- Server/main.go
+++ Server/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-	"unicode/utf8"
+	"encoding/json"
 	"fmt"
 	"net"
+	"sync"
 )
+
+type SafeChannelAddressSlice struct {
+	v   [][]*net.UDPAddr
+	mux sync.Mutex
+}
+
+type SafeQueue struct {
+	q   []*net.UDPAddr
+	mux sync.Mutex
+}
+
+var channels = SafeChannelAddressSlice{v: [][]*net.UDPAddr{}}
+var queue = SafeQueue{q: []*net.UDPAddr{}}
 
 //yo
 func main() {
@@ -27,32 +41,90 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	defer udpConn.Close()
 	//Keep calling this function
-	for {
-		display(udpConn)
-	}
-
+	display(udpConn)
 }
 
 func display(conn *net.UDPConn) {
-
-	var buf [2048]byte
-	n, err := conn.Read(buf[0:])
-	fmt.Println(n);
-	if err != nil {
-		fmt.Println("Error Reading")
-		return
-	} else {
-		message := "";
-		totSize := 0;
-		for i:=0; i<n; i++ {
-			r, size := utf8.DecodeRune(buf[i:n])
-			message+= string(r)
-			totSize += size;
+	for {
+		var buf [1024]byte
+		n, addr, err := conn.ReadFromUDP(buf[0:])
+		fmt.Println(n)
+		if err != nil {
+			fmt.Println("Error Reading")
+			return
+		} else {
+			go handleMessage(conn, buf, n, addr)
 		}
-		fmt.Println(message)
-		fmt.Println("Package Done")
 	}
+}
 
+func handleMessage(conn *net.UDPConn, buf [1024]byte, n int, addr *net.UDPAddr) {
+	var result map[string]interface{}
+
+	json.Unmarshal(buf[0:n], &result)
+
+	switch res := result["type"]; res {
+	case "transform":
+		fmt.Println("handle tranform")
+		handleTransform(conn, result, addr)
+	case "queue":
+		fmt.Println("handle queue")
+		handleQueue(conn, result, n, addr)
+	}
+}
+
+func handleTransform(conn *net.UDPConn, result map[string]interface{}, addr *net.UDPAddr) {
+	fmt.Println(result["channel"])
+	channel := int(result["channel"].(float64))
+	addressesToSend := channels.v[channel]
+
+	bytes, err := json.Marshal(result)
+	if err != nil {
+		fmt.Println("Can't serislize", result)
+	}
+	for _, address := range addressesToSend {
+		if address != addr {
+			conn.WriteTo(bytes, address)
+		}
+	}
+}
+
+func handleQueue(conn *net.UDPConn, result map[string]interface{}, n int, addr *net.UDPAddr) {
+	queue.mux.Lock()
+	queue.q = append(queue.q, addr)
+
+	channels.mux.Lock()
+
+	if len(queue.q) > 1 {
+		for {
+			newChannelindex := len(channels.v)
+			channels.v = append(channels.v, []*net.UDPAddr{queue.q[0], queue.q[1]})
+
+			sendObj := make(map[string]interface{})
+
+			sendObj["channel"] = newChannelindex
+			sendObj["type"] = "newGame"
+
+			bytes, err := json.Marshal(sendObj)
+			if err != nil {
+				fmt.Println("Can't serislize", sendObj)
+			}
+
+			conn.WriteTo(bytes, queue.q[0])
+			conn.WriteTo(bytes, queue.q[1])
+
+			queue.q = queue.q[2:]
+
+			if len(queue.q) < 2 {
+				break
+			}
+		}
+	}
+	fmt.Println(channels.v)
+	fmt.Println(queue.q)
+	channels.mux.Unlock()
+
+	queue.mux.Unlock()
 }
