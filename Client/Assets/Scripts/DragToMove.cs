0791072d37a293671cdff97af2e77732f049c2e7
Håvard Markhus <haavasma@stud.ntnu.no>
2020-11-13 23:50:27 +0100

    finished first iteration of gameplay mechanic, will still be WIP

--- Client/Assets/Scripts/DragToMove.cs
+++ Client/Assets/Scripts/DragToMove.cs
@@ -11,8 +11,18 @@ public class DragToMove : MonoBehaviour
 
     private Rigidbody rigidbody;
 
+    private bool falling = true;
+
+    [SerializeField] float fallSpeed = 0.01f;
+
     void Start() {
         rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        if(falling) {
+            rigidbody.MovePosition(new Vector3(transform.position.x, transform.position.y - fallSpeed, transform.position.z));
+        }
     }
 
     void OnMouseDown()
@@ -35,6 +45,15 @@ public class DragToMove : MonoBehaviour
 
     void OnMouseUp() { 
         rigidbody.useGravity = true;
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if(falling && Time.time >= 0.05f) {
+            Debug.Log(Time.time);
+            Debug.Log("Collided");
+            rigidbody.useGravity = true;
+            falling = false;
+        }
     }
 
 }
--- Client/Assets/Scripts/GameController.cs
+++ Client/Assets/Scripts/GameController.cs
@@ -6,43 +6,93 @@ public class GameController : MonoBehaviour
 {
     public GameObject Piece;
 
+    [SerializeField] float fallSpeed = 0.000001f;
+
     private GameObject focus;
 
     private GameObject cubes;
+
+    private List<GameObject> pieces;
 
     // Start is called before the first frame update
     void Start()
     {
         cubes = GameObject.FindWithTag("CubeHolder");
         focus = GameObject.FindWithTag("CameraFocus");
-        StartCoroutine("Spawn");
-    }
+        pieces = new List<GameObject>();
+        //StartCoroutine("Spawn");
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    IEnumerator Spawn()
-    {
-        for (int i = 0; i < 33; i++)
+        for (int i = 0; i < 54; i++)
         {
-            Debug.Log(Piece.transform.localScale);
             Vector3 spawnPoint = new Vector3();
             Quaternion rotation = Quaternion.identity;
 
             if (Mathf.Ceil(i / 3) % 2 == 0)
             {
                 spawnPoint = new Vector3(0.0f,
-                                        Mathf.Ceil(i / 3) * Piece.transform.localScale.y,
+                                        Mathf.Ceil(i / 3) * Piece.transform.localScale.y * 1.5f,
+                                        (((i % 3) * Piece.transform.localScale.z) - Piece.transform.localScale.z) * 1.0f);
+                spawnPoint += focus.transform.position;
+            }
+            else
+            {
+                spawnPoint = new Vector3((((i % 3) * Piece.transform.localScale.z) - Piece.transform.localScale.z) * 1.0f,
+                                        Mathf.Ceil(i / 3) * Piece.transform.localScale.y * 1.5f,
+                                        0.0f);
+                spawnPoint += focus.transform.position;
+                rotation = Quaternion.Euler(0, 90, 0);
+            }
+            GameObject piece = Instantiate(Piece, spawnPoint, rotation);
+
+            piece.GetComponent<Rigidbody>().useGravity = false;
+
+            piece.transform.parent = cubes.transform;
+
+            pieces.Add(piece);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        float step = fallSpeed * Time.deltaTime;
+        for (int i = 0; i < pieces.Count; i++)
+        {
+            Vector3 targetPos =  new Vector3(pieces[i].transform.position.x,
+                                                                    Mathf.Ceil(i / 3) * Piece.transform.localScale.y,
+                                                                    pieces[i].transform.position.z
+                                                                );   
+            if(pieces[i].transform.position.y <= targetPos.y) {
+                pieces[i].GetComponent<Rigidbody>().useGravity = true;
+                continue;
+            }
+            Debug.Log(targetPos);
+            Debug.Log(pieces[i].transform.position);
+            Debug.Log("");
+            pieces[i].transform.position = Vector3.MoveTowards(pieces[i].transform.position, targetPos, step);
+        }
+        */
+    }
+
+    IEnumerator Spawn()
+    {
+        for (int i = 0; i < 33; i++)
+        {
+            Vector3 spawnPoint = new Vector3();
+            Quaternion rotation = Quaternion.identity;
+
+            if (Mathf.Ceil(i / 3) % 2 == 0)
+            {
+                spawnPoint = new Vector3(0.0f,
+                                        Mathf.Ceil(i / 3) * Piece.transform.localScale.y + 0.5f * Piece.transform.localScale.y,
                                         ((i % 3) * Piece.transform.localScale.z) - Piece.transform.localScale.z);
                 spawnPoint += focus.transform.position;
             }
             else
             {
                 spawnPoint = new Vector3(((i % 3) * Piece.transform.localScale.z) - Piece.transform.localScale.z,
-                                        Mathf.Ceil(i / 3) * Piece.transform.localScale.y,
+                                        Mathf.Ceil(i / 3) * Piece.transform.localScale.y + 0.5f * Piece.transform.localScale.y,
                                         0.0f);
                 spawnPoint += focus.transform.position;
                 rotation = Quaternion.Euler(0, 90, 0);
@@ -50,8 +100,9 @@ public class GameController : MonoBehaviour
             GameObject piece = Instantiate(Piece, spawnPoint, rotation);
 
             piece.transform.parent = cubes.transform;
-            
-            if(i%3 == 2) {
+
+            if (i % 3 == 2)
+            {
                 yield return new WaitForSeconds(1f);
             }
         }
